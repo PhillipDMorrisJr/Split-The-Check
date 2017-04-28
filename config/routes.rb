c0d172ec3e6367055388a42724567944a7340e3d
Nicholas McWilliams <student@gowolves>
2017-04-28 00:00:48 -0400

    Changed new restaurant button_to back to link_to as it brought the program to its knees and was unable to create restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+
   resources :restaurants do
+	post 'new' => 'restaurants#create'
    get 'show' => 'restaurants#show'
    member do
+
     patch 'upvote' => 'restaurants#upvote'
     patch 'downvote' => 'restaurants#downvote'
    end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426183244) do
+ActiveRecord::Schema.define(version: 20170427231458) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170426183244) do
     t.integer  "down_vote"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
