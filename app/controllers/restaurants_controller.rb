1603f0af7789c5a2bc343acff068a679553ccba4
student <student@gowolves>
2017-05-09 04:27:04 -0400

    I am stuck and stomped

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,6 +9,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants.json
   def index
     @restaurants = Restaurant.all
+    @vote = VoteHistory.all
     if params[:search]
       @restaurants = Restaurant.search(params[:search])
     end
@@ -22,6 +23,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
+    @vote = VoteHistory.new
   end
 
   # GET /restaurants/1/edit
@@ -46,14 +48,14 @@ class RestaurantsController < ApplicationController
 
   
   def upvote
-    @vote = Vote.new(upvote_params)
+    @vote = VoteHistory.new(upvote_params)
     @restaurant.save!
     redirect_back(fallback_location: restaurants_path)
   end
 
   
   def downvote
-    @vote = Vote.new(downvote_params)
+    @vote = VoteHistory.new(downvote_params)
     @restaurant.save!
     redirect_back(fallback_location: restaurants_path)
   end
@@ -93,9 +95,9 @@ class RestaurantsController < ApplicationController
       params.require(:restaurant).permit(:name, :location, :up_vote, :down_vote)
     end
     def upvote_params
-      params.require(:vote).permit(:user, :restaurant, true)
+      params.permit(:user, :restaurant, true)
     end
     def downvote_params
-      params.require(:vote).permit(:user, :restaurant, false)
+      params.permit(:user, :restaurant, false)
     end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,17 +1,20 @@
 class Restaurant < ApplicationRecord
 	
-	validates :name, :location presence: true
+	validates :name,:location, :up_vote, :down_vote, presence: true
 	belongs_to :user
+        has_many :vote_histories
 
 	def self.search(search)
 		where("name LIKE ? OR location LIKE ?", "%#{search}%", "%#{search}%")
 	end
-
+        def new
+         @vote_histories = VoteHistory.new
+        end
   	def upvote
          count = 0
-         @vote_histories.each do |vote|
+         @vote_histories.all.each do |vote|
           if(vote.upvote)
-           count = count + 1
+           up_vote = up_vote + 1
           end
 	  return count
          end
@@ -21,7 +24,7 @@ class Restaurant < ApplicationRecord
   
   	def downvote
          count = 0
-         @vote_histories.each do |vote|
+         @vote_histories.all.each do |vote|
           if(vote.upvote == false)
            count = count - 1
           end
